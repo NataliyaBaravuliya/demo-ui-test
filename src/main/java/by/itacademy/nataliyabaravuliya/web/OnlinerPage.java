a2c56d362b27a6aba4d5cbfe104c935472f0b883
NataliyaBaravuliya <NataliyaBaravuliya@gmail.com>
2023-02-13 16:06:11 +0300

    refactor onlinerpage class refactor testopenonlinerloginform add testonlinerloginformwithemptycredentials

--- src/main/java/by/itacademy/nataliyabaravuliya/web/OnlinerPage.java
+++ src/main/java/by/itacademy/nataliyabaravuliya/web/OnlinerPage.java
@@ -5,4 +5,7 @@ public class OnlinerPage {
         public static final String URL = "https://www.onliner.by/";
         public static final String  COPYRIGHT =  "/html/body/div[4]/footer/div/div/div/div[2]/div[2]";
         public static final String BTN_LOGIN = "/html/body/div[4]/div/div/div/header/div[3]/div/div[2]/div[2]/div[1]/div/div/div[1]";
+
+        public static final String BTN_ENTER  = "/html/body/div[6]/div/div/div/div/div/div/div/div[2]/div/form/div[3]/button";
+        public static final String LABEL = "//*[@id=\"auth-container\"]/div/div[2]/div/div[1]";
 }
--- src/test/OnlinerTest.java
+++ src/test/OnlinerTest.java
@@ -6,27 +6,45 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-
-    public class OnlinerTest {
-        @Test
-        public void testOpenOnliner() {
-            ChromeDriver driver = new ChromeDriver();
-            driver.get(OnlinerPage.URL);
-            By copyright = By.xpath(OnlinerPage.COPYRIGHT);
-            WebElement element = driver.findElement(copyright);
-            Assert.assertTrue(element.isDisplayed());
-            driver.quit();
-        }
-
-        @Test
-        public void testOpenOnlinerLoginForm() {
-            ChromeDriver driver = new ChromeDriver();
-            driver.get(OnlinerPage.URL);
-            By btn_login = By.xpath(OnlinerPage.BTN_LOGIN);
-            WebElement element = driver.findElement(btn_login);
-            Assert.assertTrue(element.isDisplayed());
-            element.click();
-            driver.quit();
-        }
+public class OnlinerTest {
+    @Test
+    public void testOpenOnliner() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get(OnlinerPage.URL);
+        By copyright = By.xpath(OnlinerPage.COPYRIGHT);
+        WebElement element = driver.findElement(copyright);
+        Assert.assertTrue(element.isDisplayed());
+        driver.quit();
     }
+
+    @Test
+    public void testOpenOnlinerLoginForm() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get(OnlinerPage.URL);
+        By btn_login = By.xpath(OnlinerPage.BTN_LOGIN);
+        WebElement element = driver.findElement(btn_login);
+        element.click();
+        By label = By.xpath(OnlinerPage.LABEL);
+        WebElement element1 = driver.findElement(label);
+        Assert.assertTrue(element1.isDisplayed());
+        driver.quit();
+    }
+
+    @Test
+    public void testOnlinerLoginFormWithEmptyCredentials() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get(OnlinerPage.URL);
+        By btn_login = By.xpath(OnlinerPage.BTN_LOGIN);
+        WebElement element = driver.findElement(btn_login);
+        element.click();
+        By btn_enter = By.xpath(OnlinerPage.BTN_ENTER);
+        WebElement element2 = driver.findElement(btn_enter);
+        element2.click();
+        //Assert.assertTrue(element1.isDisplayed());
+        // driver.quit();
+    }
+
+
+    //element1.sendKeys("test@gmail.com");
+}
 
