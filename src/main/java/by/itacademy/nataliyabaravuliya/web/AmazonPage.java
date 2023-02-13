de84bd2bbf5333632cc9c0a5e989032f9ee1379a
NataliyaBaravuliya <NataliyaBaravuliya@gmail.com>
2023-02-13 22:22:05 +0300

    refactor amazonpage class add testOpenAmazonCart

--- src/main/java/by/itacademy/nataliyabaravuliya/web/AmazonPage.java
+++ src/main/java/by/itacademy/nataliyabaravuliya/web/AmazonPage.java
@@ -3,8 +3,8 @@ package by.itacademy.nataliyabaravuliya.web;
 public class AmazonPage {
     public static final String URL = "https://www.amazon.com/";
     public static final String COPYRIGHT =  "//*[@id=\"navFooter\"]/div[5]/span";
-    public static final String BTN_LOGIN = "";
+    public static final String BTN_CART = "//*[@id=\"nav-cart\"]";
     public static final String BTN_ENTER  = "";
-    public static final String LABEL = "";
+    public static final String LABEL = "//*[@id=\"sc-active-cart\"]/div/div/div[2]/div[1]/h2";
     public static final String INPUT = "";
 }
--- src/test/by/itacademy/nataliyabaravuliya/web/AmazonTest.java
+++ src/test/by/itacademy/nataliyabaravuliya/web/AmazonTest.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class AmazonTest{
+public class AmazonTest {
     @Test
-    public void testOpenAmazon(){
+    public void testOpenAmazon() {
         ChromeDriver driver = new ChromeDriver();
         driver.get(AmazonPage.URL);
         By copyright = By.xpath(AmazonPage.COPYRIGHT);
@@ -16,5 +16,17 @@ public class AmazonTest{
         Assert.assertTrue(COPYRIGHT.isDisplayed());
         driver.quit();
 
+    }
+    @Test
+    public void testOpenAmazonCart() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get(AmazonPage.URL);
+        By btn_login = By.xpath(AmazonPage.BTN_CART);
+        WebElement BTN_CART = driver.findElement(btn_login);
+        BTN_CART.click();
+        By label = By.xpath(AmazonPage.LABEL);
+        WebElement LABEL = driver.findElement(label);
+        Assert.assertTrue(LABEL.isDisplayed());
+        driver.quit();
     }
 }
