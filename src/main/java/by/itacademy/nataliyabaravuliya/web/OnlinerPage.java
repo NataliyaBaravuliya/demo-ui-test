3f94024df722637d665e563831e6f8a49ffcc64b
NataliyaBaravuliya <NataliyaBaravuliya@gmail.com>
2023-02-13 12:36:26 +0300

    add testOpenOnlinerLoginForm

--- src/main/java/by/itacademy/nataliyabaravuliya/web/OnlinerPage.java
+++ src/main/java/by/itacademy/nataliyabaravuliya/web/OnlinerPage.java
@@ -4,5 +4,5 @@ package by.itacademy.nataliyabaravuliya.web;
 public class OnlinerPage {
         public static final String URL = "https://www.onliner.by/";
         public static final String  COPYRIGHT =  "/html/body/div[4]/footer/div/div/div/div[2]/div[2]";
-        public static final String BTN_LOGIN = "/html/body/div[3]/div/div/div/header/div[3]/div/div[2]/div[2]/div[1]/div/div/div[2]";
+        public static final String BTN_LOGIN = "/html/body/div[4]/div/div/div/header/div[3]/div/div[2]/div[2]/div[1]/div/div/div[1]";
 }
--- src/test/OnlinerTest.java
+++ src/test/OnlinerTest.java
@@ -5,15 +5,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class OnlinerTest {
-    @Test
-    public void testOpenOnliner() {
 
-        ChromeDriver driver = new ChromeDriver();
-        driver.get(OnlinerPage.URL);
-        By copyright = By.xpath(OnlinerPage.COPYRIGHT);
-        WebElement element = driver.findElement(copyright);
-        Assert.assertTrue(element.isDisplayed());
-        driver.quit();
+
+    public class OnlinerTest {
+        @Test
+        public void testOpenOnliner() {
+            ChromeDriver driver = new ChromeDriver();
+            driver.get(OnlinerPage.URL);
+            By copyright = By.xpath(OnlinerPage.COPYRIGHT);
+            WebElement element = driver.findElement(copyright);
+            Assert.assertTrue(element.isDisplayed());
+            driver.quit();
+        }
+
+        @Test
+        public void testOpenOnlinerLoginForm() {
+            ChromeDriver driver = new ChromeDriver();
+            driver.get(OnlinerPage.URL);
+            By btn_login = By.xpath(OnlinerPage.BTN_LOGIN);
+            WebElement element = driver.findElement(btn_login);
+            Assert.assertTrue(element.isDisplayed());
+            element.click();
+            driver.quit();
+        }
     }
-}
+
